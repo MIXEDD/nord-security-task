bc4288ebe2b9fd818f0d1d97a9efb9bfbb96f77e
Maksimilian <maksimilian.ceresniovyj@locatory.com>
2022-07-18 18:26:24 +0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 resolve: {
