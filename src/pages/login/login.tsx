93be79176b392407695c56ea54e2ac6660c63b4f
Maksimilian <maksimilian.ceresniovyj@locatory.com>
2022-07-19 10:14:41 +0300

    Added Basic typography component

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from '../../atoms/button/button';
 import InputField from '../../atoms/input-field/input-field';
 import Grid, { AlignItems, FlexDirection } from '../../atoms/grid/grid';
+import Typography, { ElementType, TextAlign } from '../../atoms/typography/typography';
 
 import styles from './login.scss';
 
@@ -18,6 +19,11 @@ const Login: React.FC = () => {
     return (
         <Grid direction={FlexDirection.Column} alignItems={AlignItems.Center}>
             <div className={styles.login}>
+                <Typography
+                    elementType={ElementType.H1}
+                    textAlign={TextAlign.Center}
+                    text="Nord Security"
+                />
                 <Grid direction={FlexDirection.Column}>
                     <InputField onChangeInput={onChangeUsername} name="username" label="Username" />
                 </Grid>
