92a5f5661ed166e93abf799bc7bae6bb47f73922
Maksimilian <maksimilian.ceresniovyj@locatory.com>
2022-07-19 08:42:46 +0300

    Add button type

--- src/atoms/button/button.tsx
+++ src/atoms/button/button.tsx
@@ -2,18 +2,35 @@ import classnames from 'classnames';
 import React from 'react';
 import styles from './styles.scss';
 
+export enum ButtonType {
+    Primary = 'PRIMARY',
+}
+
 interface Props {
     text: string;
     onClick: () => void;
     disabled?: boolean;
+    buttonType?: ButtonType;
 }
 
+const getButtonTypeStyles = (buttonType: ButtonType) => {
+    if (buttonType === ButtonType.Primary) {
+        return styles.primary;
+    }
+
+    return null;
+};
+
 const Button: React.FC<Props> = (props) => {
-    const { text, onClick, disabled } = props;
+    const { text, onClick, disabled, buttonType = ButtonType.Primary } = props;
 
     return (
         <button
-            className={classnames(styles.button, disabled && styles.disabled)}
+            className={classnames(
+                styles.button,
+                disabled && styles.disabled,
+                getButtonTypeStyles(buttonType),
+            )}
             onClick={onClick}
             disabled={disabled}
         >
