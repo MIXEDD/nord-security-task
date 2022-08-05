5ef78a0a24fc0cd143499563c6c6a79840edcc82
Maksimilian <maksimilian.ceresniovyj@locatory.com>
2022-08-05 11:18:39 +0300

    Button tests

--- src/atoms/button/button.spec.tsx
+++ src/atoms/button/button.spec.tsx
@@ -1,8 +1,31 @@
 import Button from './button';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('Button', () => {
     it('Should render', () => {
         render(<Button text="test" onClick={() => {}} />);
+    });
+
+    it('Should check button text', () => {
+        render(<Button text="test" onClick={() => {}} />);
+
+        expect(screen.getByText('test')).toBeTruthy();
+    });
+
+    it('Should check if button is clicked', async () => {
+        const onClick = jest.fn();
+
+        render(<Button text="test" onClick={onClick} />);
+
+        await userEvent.click(screen.getByText('test'));
+
+        expect(onClick).toHaveBeenCalled();
+    });
+
+    it('Should button be disabled', () => {
+        render(<Button text="test" onClick={() => {}} disabled />);
+
+        expect(screen.getByText('test')).toBeDisabled();
     });
 });
